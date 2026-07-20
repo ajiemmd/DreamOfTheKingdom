db5bb17f60645e8158642cb633e83d950136ee5a
unknown <287619460@qq.com>
2026-07-20 13:27:42 +0800

    初次提交，创建项目导入资源，创建第一个预制体

---
+++ Assets/Scripts/Room/MonoBehaviour/Room.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int column;
+    public int line;
+
+
+    private SpriteRenderer spriteRenderer;
+
+    public RoomDataSO roomData;
+
+    public RoomState roomstate;
+
+    private void Awake()
+    {
+        spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetupRoom(0, 0, roomData);
+    }
+
+    private void OnMouseDown()
+    {
+        // 处理点击事件
+        Debug.Log("点击了房间：" + roomData.roomType);
+    }
+
+
+    /// <summary>
+    /// 外部创建房间时调用此函数，初始化
+    /// </summary>
+    /// <param name="column"></param>
+    /// <param name="line"></param>
+    /// <param name="roomData"></param>
+    public void SetupRoom(int column, int line, RoomDataSO roomData)
+    {
+        this.column = column;
+        this.line = line;
+        this.roomData = roomData;
+
+        spriteRenderer.sprite = roomData.roomIcon;
+
+    }
+
+}
---
+++ Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,17 @@
+public enum RoomType
+{
+    MinoEnemy,
+    EliteEnemy,
+    Boss,
+    Shop,
+    Treasure,
+    RestRoom
+}
+
+
+public enum RoomState
+{
+    Locked,
+    Visited,
+    Attainable
+}
